c88b687ecbae259561efb12e4e4c8871f08b712a
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-04-17 11:05:49 +0530

    add: loading, error page

--- src/pages/sharedTrans.tsx
+++ src/pages/sharedTrans.tsx
@@ -34,16 +34,24 @@ const SharedTrans = () => {
     }, [token]);
 
     return (
-        <div>
+        <div className={`${error ? "bg-white" : data.length > 0 ? "bg-gray-200" : "bg-[#fcfbed]"}` + " min-h-screen"}>
             <header className="w-full bg-[#4FB92D] sm:px-10 px-6 sm:py-4 py-2 sticky top-0">
                 <h1 className="text-white font-semibold sm:text-3xl text-lg">₹ Rupayie</h1>
             </header>
 
             <>
                 {error ? (
-                    <p className="text-red-600 font-medium p-10">{error}</p>
-                ) : data && (
-                    <TransactionsDetails data={data} />
+                    <div className="w-full flex flex-row justify-center items-center">
+                        <img className="w-[75%]" src="https://freefrontend.com/assets/img/500-error-page-html-templates/Error-500-alert.gif" alt="" />
+                    </div>
+                ) : data.length > 0 ? (
+                    <>
+                        <TransactionsDetails data={data} />
+                    </>
+                ) : (
+                    <div className="w-full flex flex-row justify-center items-center sm:mt-36 mt-10">
+                        <img className="sm:w-1/4 w-[60%]" src="https://cdn.dribbble.com/userupload/19248258/file/original-14b7591104f6dde68b8526dcddb6f02f.gif" alt="" />
+                    </div>
                 )}
             </>
         </div>
